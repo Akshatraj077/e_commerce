aa9727959a9513c48a396761c241519e2276e11a
Akshat Raj <rajakshat077@gmail.com>
2025-08-20 13:57:34 +0530

    Removed group-hover:block hidden in Navbar profile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             {/* Dropdown icon */}
             {
               token && open &&
-              <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+              <div className='absolute dropdown-menu right-0 pt-4'>
                 <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                   <p className='cursor-pointer hover:text-black'>My Profile</p>
                   <p onClick={() => navigate('/Orders')} className='cursor-pointer hover:text-black'>Orders</p>
